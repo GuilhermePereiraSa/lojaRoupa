38cceeffd4138526467983f73776f41b44cf38cb
GuilhermePereiraSa <guilherme1478.wrath@gmail.com>
2026-06-04 14:38:09 -0300

    fix path login in init.js

--- frontend/scripts/init.js
+++ frontend/scripts/init.js
@@ -51,7 +51,7 @@ class AuthConfig {
     } else {
       const LogIn = document.createElement("li");
       // Caminho absoluto com barra
-      LogIn.innerHTML = `<a href="/frontend/pages/login.html">Entrar</a>`;
+      LogIn.innerHTML = `<a href="/pages/login.html">Entrar</a>`;
       navBar.appendChild(LogIn);
     }
   }
@@ -59,7 +59,7 @@ class AuthConfig {
   logOut() {
     localStorage.removeItem("Token");
     // Caminho absoluto para o redirecionamento funcionar em qualquer tela
-    window.location.href = "/frontend/pages/login.html";
+    window.location.href = "/pages/login.html";
   }
 }
 
