e2c9ad89d689c058a9140142deb2b6e970be51ab
GuilhermePereiraSa <guilherme1478.wrath@gmail.com>
2026-06-07 04:15:24 -0300

    fixes service as email

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -233,8 +233,7 @@ export const forgotPassword = async (req, res) => {
     });
 
     const transporter = nodemailer.createTransport({
-      host: process.env.EMAIL_HOST,
-      port: process.env.EMAIL_PORT,
+      service: "gmail",
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
