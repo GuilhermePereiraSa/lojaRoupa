358fff46fb128ab7812f1c8f9717a01593be3b95
GuilhermePereiraSa <guilherme1478.wrath@gmail.com>
2026-06-07 21:01:16 -0300

    added routes cart

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import sequelize from "./src/models/dbconfig.js";
 import orderRouter from "./src/routes/order.routes.js";
 import authRouter from "./src/routes/auth.routes.js";
 import clothingRouter from "./src/routes/clothing.routes.js";
+import cartRouter from "./src/routes/cart.routes.js";
 
 // enxergar tables
 import "./src/models/user.model.js";
@@ -21,6 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/auth", authRouter);
 app.use("/api/produtos", clothingRouter);
 app.use("/api/pedidos", orderRouter);
+app.use("/api/cart", cartRouter);
 
 app.get("/health", (req, res) => {
   res
